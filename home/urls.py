0e08a2da481cdd9433322475ee9bcd84b8fa4d75
Tricia <tricia.alves@acad.ufsm.br>
2026-04-02 21:31:13 -0300

    interfaces

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, PainelView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('painel', PainelView.as_view(), name='painel'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+class PainelView(TemplateView):
+    template_name = 'painel.html'
